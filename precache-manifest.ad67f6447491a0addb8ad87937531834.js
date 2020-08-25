6318ba5a7e994dee89a12d2230adf96b60cf6d50
usamafarooq169 <usamafarooq459@gmail.com>
2020-08-25 12:41:42 +0500

    Updates

---
+++ precache-manifest.ad67f6447491a0addb8ad87937531834.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4e81e4ee58b6e087ec5bc5f89976cf",
+    "url": "/ipfs-eth-fileupload/index.html"
+  },
+  {
+    "revision": "4c633d684081f0b62035",
+    "url": "/ipfs-eth-fileupload/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "60e0b25a6229b6393436",
+    "url": "/ipfs-eth-fileupload/static/css/main.3b740b0c.chunk.css"
+  },
+  {
+    "revision": "4c633d684081f0b62035",
+    "url": "/ipfs-eth-fileupload/static/js/2.83dfd191.chunk.js"
+  },
+  {
+    "revision": "c6bed1d135c9591e310a556900a6020a",
+    "url": "/ipfs-eth-fileupload/static/js/2.83dfd191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60e0b25a6229b6393436",
+    "url": "/ipfs-eth-fileupload/static/js/main.b457c7a9.chunk.js"
+  },
+  {
+    "revision": "17e987770711070005ec",
+    "url": "/ipfs-eth-fileupload/static/js/runtime-main.280ab5d7.js"
+  }
+]);
