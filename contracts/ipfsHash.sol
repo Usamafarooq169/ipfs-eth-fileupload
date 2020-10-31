c0bf335636c5c529bc116d381f2f4a1ae8af890e
Usamafarooq169 <56949404+Usamafarooq169@users.noreply.github.com>
2020-10-31 07:54:09 +0500

    Update ipfsHash.sol

--- contracts/ipfsHash.sol
+++ contracts/ipfsHash.sol
@@ -36,4 +36,26 @@ contract Contract
     // {
     //     return ipfsHashes;
     // }
+    
+    //Code 3 (Notarization)
+    // pragma solidity ^0.4.4;
+// contract Notary {
+// struct Record {
+//     uint mineTime;
+//     uint blockNumber;
+//   }
+// mapping (bytes32 => Record) private docHashes;
+// function Notary() public {
+//     // constructor
+//   }
+// function addDocHash (bytes32 hash) public {
+//     Record memory newRecord = Record(now, block.number);
+//     docHashes[hash] = newRecord;
+//   }
+// function findDocHash (bytes32 hash) public constant returns(uint, uint) {
+//     return (docHashes[hash].mineTime, docHashes[hash].blockNumber);
+//   }
+// }
+
+    
 }
